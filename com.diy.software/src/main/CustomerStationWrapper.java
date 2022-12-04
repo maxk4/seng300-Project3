69bae0e75df3bc07b2d25ee2f222db3759915bfc
etorresh <me@etorresh.com>
2022-12-03 20:22:32 -0700

    changes before merge

--- com.diy.software/src/main/CustomerStationWrapper.java
+++ com.diy.software/src/main/CustomerStationWrapper.java
@@ -6,7 +6,7 @@ import com.diy.hardware.PLUCodedProduct;
 import com.diy.hardware.Product;
 
 import cart.CartController;
-import cart.ItemAddedListener;
+import cart.CartListener;
 import payment.PaymentController;
 import payment.PaymentListener;
 import printing.PrinterController;
@@ -17,6 +17,7 @@ import ui.AttendantUIListener;
 import ui.CustomerUI;
 import ui.CustomerUIListener;
 import util.Bag;
+import util.ProductInfo;
 
 public class CustomerStationWrapper {
 	
@@ -26,16 +27,124 @@ public class CustomerStationWrapper {
 	private ScaleController scale;
 	private CustomerUI customer;
 	private boolean inProgress = true;
+	
+	
 	private Product waitingFor = null;
 	private String waitingForDescription = null;
-
 	
-	private PaymentListener paymentListener;
-	private ItemAddedListener cartListener;
-	private ScaleListener scaleListener;
-	private CustomerUIListener customerUIListener;
+	
 	
 	public CustomerStationWrapper(DoItYourselfStation diyStation, AttendantUI attendant) {
+		
+		payment = new PaymentController(diyStation);
+		cart = new CartController(diyStation);
+		print = new PrinterController(diyStation);
+		scale = new ScaleController(diyStation);
+		customer = new CustomerUI(diyStation, "Customer Station");
+		
+		payment.register(new PaymentListener() {
+			@Override
+			public void cardPaymentSucceeded() {
+				customer.setView(CustomerUI.SCAN);
+				updateProductList();
+			}
+
+			@Override
+			public void cashInserted() {
+				updateCashGUI();
+				updateProductList();
+			}
+		});
+		
+		cart.register(new CartListener() {
+
+			@Override
+			public void notifyItemAdded(Product prod, long price, double weight) {
+				payment.addCost(price);
+				updateProductList();
+				updateCashGUI();
+				if (!(prod instanceof Bag)) {
+					scale.updateExpectedWeight(weight);
+				}
+			}
+
+			@Override
+			public void notifyItemRemoved(Product prod, long price, double weight) {
+				payment.addCost(-price);
+				updateProductList();
+				updateCashGUI();
+				if (!(prod instanceof Bag))
+					scale.updateExpectedWeight(-weight);
+			}
+			
+		});
+		
+		customer.register(new CustomerUIListener() {
+			@Override
+			public void purchaseBags(int amount) {
+				for (int i = 0; i < amount; i++) {
+					//add bag
+					Bag bag = new Bag();
+					cart.addItem(bag, Bag.DESCRIPTION, Bag.PRICE, Bag.WEIGHT);
+				}
+				updateProductList();
+			}
+			
+			@Override
+			public void addPLUProduct(PLUCodedProduct product) {
+				waitingFor = product;
+				waitingForDescription = product.getDescription();
+				customer.setView(CustomerUI.PLACE_ITEM);
+			}
+			
+			@Override
+			public void endSession() {
+				if (payment.hasRemainingBalance()) return;
+				customer.setView(CustomerUI.END);
+				inProgress = false;
+				scale.setExpectedWeight(0);
+				System.out.println("Finish");
+				String receipt = cart.getReceipt();
+				print.print(receipt);
+				cart.clear();
+				updateProductList();
+			}
+
+			@Override
+			public void beginSession() {
+				inProgress = true;
+			}
+
+			@Override
+			public void selectItem(Product product, String description) {
+				waitingFor = product;
+				waitingForDescription = description;
+				customer.setView(CustomerUI.PLACE_ITEM);
+			}
+
+			@Override
+			public void itemPlaced() {
+				if (waitingFor == null) return;
+				
+				long price = waitingFor.getPrice();
+				double weight = scale.getScanningAreaWeight();
+				if (!waitingFor.isPerUnit()) price *= weight / 1000d;
+
+				customer.setView(CustomerUI.SCAN);
+				cart.addItem(waitingFor, waitingForDescription, price, weight);
+				waitingFor = null;
+			}
+
+			@Override
+			public void requestUsePersonalBag() {
+				boolean approved = attendant.approveOwnBagRequest(diyStation);
+				customer.setView(CustomerUI.SCAN);
+				if (approved) scale.approveWeight();
+				
+			}
+			
+		});
+		
 		attendant.register(new AttendantUIListener() {
 			@Override
 			public void approveWeight(DoItYourselfStation station) {
@@ -89,136 +198,46 @@ public class CustomerStationWrapper {
 			}
 
 			@Override
-			public void startupStation(DoItYourselfStation station) {
-				if(station != diyStation) return;
-				station.turnOn();
-				payment = new PaymentController(diyStation);
-				cart = new CartController(diyStation);
-				print = new PrinterController(diyStation);
-				scale = new ScaleController(diyStation);
-				customer = new CustomerUI(diyStation, "Customer Station");
-				paymentListener = new PaymentListener() {
-					@Override
-					public void cardPaymentSucceeded() {
-						customer.setView(CustomerUI.SCAN);
-						updateProductList();
-					}
-
-					@Override
-					public void cashInserted() {
-						updateCashGUI();
-						updateProductList();
-					}
-				};
-				payment.register(paymentListener);
-				
-				cartListener = (prod, price, weight) -> {
-					payment.addCost(price);
-					updateProductList();
-					updateCashGUI();
-					if (!(prod instanceof Bag))
-						scale.updateExpectedWeight(weight);
-				};
-				cart.register(cartListener);
-				
-				customerUIListener = new CustomerUIListener() {
-					@Override
-					public void purchaseBags(int amount) {
-						for (int i = 0; i < amount; i++) {
-							//add bag
-							Bag bag = new Bag();
-							cart.addItem(bag, Bag.DESCRIPTION, Bag.PRICE, Bag.WEIGHT);
-						}
-						updateProductList();
-					}
-					
-					@Override
-					public void addPLUProduct(PLUCodedProduct product) {
-						waitingFor = product;
-						waitingForDescription = product.getDescription();
-						customer.setView(CustomerUI.PLACE_ITEM);
-					}
-					
-					@Override
-					public void endSession() {
-						if (payment.hasRemainingBalance()) return;
-						customer.setView(CustomerUI.END);
-						inProgress = false;
-						scale.setExpectedWeight(0);
-						System.out.println("Finish");
-						String receipt = cart.getReceipt();
-						print.print(receipt);
-						cart.clear();
-						updateProductList();
-					}
-
-					@Override
-					public void beginSession() {
-						inProgress = true;
-					}
-
-					@Override
-					public void selectItem(Product product, String description) {
-						waitingFor = product;
-						waitingForDescription = description;
-						customer.setView(CustomerUI.PLACE_ITEM);
-					}
-
-					@Override
-					public void itemPlaced() {
-						if (waitingFor == null) return;
-						
-						long price = waitingFor.getPrice();
-						double weight = scale.getScanningAreaWeight();
-						if (!waitingFor.isPerUnit()) price *= weight;
-						
-						cart.addItem(waitingFor, waitingForDescription, price, weight);
-						customer.setView(CustomerUI.SCAN);
-						waitingFor = null;
-					}
-
-					@Override
-					public void requestUsePersonalBag() {
-						boolean approved = attendant.approveOwnBagRequest(diyStation);
-						customer.setView(CustomerUI.SCAN);
-						if (approved) scale.approveWeight();
-						
-					}
-				};
-				customer.register(customerUIListener);
-				scaleListener = new ScaleListener() {
-					@Override
-					public void notifyWeightDiscrepancyDetected() {
-						if (inProgress) {
-							customer.setView(CustomerUI.WEIGHT_DISCREPANCY);
-							attendant.notifyWeightDiscrepancyDetected(diyStation);
-						}
-					}
-
-					@Override
-					public void notifyWeightDiscrepancyResolved() {
-						if (inProgress) {
-							customer.setView(CustomerUI.SCAN);
-							attendant.notifyWeightDiscrepancyResolved(diyStation);
-						} else {
-							customer.setView(CustomerUI.START);
-							inProgress = true;
-						}
-					}
-				};
-				scale.register(scaleListener);
+			public ProductInfo[] requestProductInfo(DoItYourselfStation station) {
+				if (station == diyStation) return cart.getProductInfo();
+				return null;
 			}
 
 			@Override
-			public void shutdownStation(DoItYourselfStation station) {
-				if(station != diyStation) return;
-				// TO DO: If an attendant is in an active station the Attendant should have a button to confirm shutdown.
-				payment.deregister(paymentListener);
-				cart.deregister(cartListener);
-				scale.deregister(scaleListener);
-				customer.deregister(customerUIListener);
-				station.turnOff();
+			public void addItem(DoItYourselfStation station, Product product, String description) {
+				if (station != diyStation) return;
+				waitingFor = product;
+				waitingForDescription = description;
+				customer.setView(CustomerUI.PLACE_ITEM);
 			}
+
+			@Override
+			public void removeItem(DoItYourselfStation station, Product product, String description, long price, double weight) {
+				if (station != diyStation) return;
+				cart.removeItem(product, description, price, weight);
+			}
+		});
+		
+		scale.register(new ScaleListener() {
+			@Override
+			public void notifyWeightDiscrepancyDetected() {
+				if (inProgress) {
+					customer.setView(CustomerUI.WEIGHT_DISCREPANCY);
+					attendant.notifyWeightDiscrepancyDetected(diyStation);
+				}
+			}
+
+			@Override
+			public void notifyWeightDiscrepancyResolved() {
+				if (inProgress) {
+					customer.setView(CustomerUI.SCAN);
+					attendant.notifyWeightDiscrepancyResolved(diyStation);
+				} else {
+					customer.setView(CustomerUI.START);
+					inProgress = true;
+				}
+			}
+			
 		});
 	}
 	
