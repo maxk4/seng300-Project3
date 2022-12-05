abd5f8dc896109665b343d0dc69152582de8ef31
taylor.wong2 <taylor.wong2@cs.ucalgary.ca>
2022-12-04 19:07:49 -0700

    Moved Membership scanning and swiping out of ui

--- com.diy.software/src/simulation/CustomerUISimulator.java
+++ com.diy.software/src/simulation/CustomerUISimulator.java
@@ -1,12 +1,17 @@
 package simulation;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Currency;
 import java.util.Locale;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,13 +20,18 @@ import javax.swing.JTextField;
 import com.diy.hardware.DoItYourselfStation;
 import com.diy.simulation.Customer;
 import com.jimmyselectronics.Item;
+import com.jimmyselectronics.necchi.Barcode;
+import com.jimmyselectronics.necchi.BarcodedItem;
 import com.jimmyselectronics.opeechee.Card;
 import com.unitedbankingservices.DisabledException;
 import com.unitedbankingservices.TooMuchCashException;
 import com.unitedbankingservices.banknote.Banknote;
 import com.unitedbankingservices.coin.Coin;
 
+import cart.ScanItemListener;
+import ui.CustomerUI;
 import util.IntegerOnlyDocument;
+import util.MembershipDatabase;
 
 public class CustomerUISimulator{
 
@@ -177,11 +187,129 @@ public class CustomerUISimulator{
 			cash.add(coin);
 		}
 		
+
+		JPanel membership = new JPanel();
+		membership.setLayout(new BoxLayout(membership, BoxLayout.PAGE_AXIS));
+		
+		//Labels used
+		JLabel label_MemberCards_Text = new JLabel("Current Membership Cards in Customer Wallet:");
+		label_MemberCards_Text.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+
+		JLabel label_Barcode_Text = new JLabel("Current Barcodes in Customer Wallet:");
+		label_Barcode_Text.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+
+		JLabel label_separator1 = new JLabel("=====================================");
+		label_separator1.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		JLabel label_separator2 = new JLabel("=====================================");
+		label_separator2.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		JLabel label_separator3 = new JLabel("=====================================");
+		label_separator3.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+
+		//Setting the combo boxes for membership cards, membership barcodes (start)
+		//get the card from the customer wallet
+		ArrayList<String> memCardsList = new ArrayList<>();
+		for (Card card : customer.wallet.cards) {
+			if(card.kind.equals("Membership"))
+			{
+				memCardsList.add(card.cardholder + " , " + card.number);
+			}
+			//Loaded the list with the membership cards
+		}
+		//Display the cards in a GUI
+		JComboBox<String> comboBox_MemberCardsInWallet = new JComboBox<String>(memCardsList.toArray(new String[memCardsList.size()]));
+		comboBox_MemberCardsInWallet.setFont(new Font("Lucida Grande", Font.BOLD, 15));
+
+		//Barcodes for membership cards
+		ArrayList<String> barcodes = new ArrayList<>();
+		for (Barcode memberBarcode : MembershipDatabase.MEMBER_BARCODES.keySet())
+		{
+			barcodes.add(memberBarcode.toString());
+		}
+		JComboBox<String> comboBox_MemberCard_Barcodes = new JComboBox<String>(barcodes.toArray(new String[memCardsList.size()]));
+		comboBox_MemberCard_Barcodes.setFont(new Font("Lucida Grande", Font.BOLD, 15));
+		//Added in 3rd Iteration ends
+				//Cancel Button (ends)
+
+				//Scan Card Button (starts)
+				/*
+				 * Scan Card button, once pressed, gets the value from the combo box field, gets the barcode from the barcode list from Simulation class, scans that using the barcode scanner using the scan() method, then checks if that exists in database and display the result on scan GUI
+				 */
+		JButton button_ScanCard = new JButton("Scan Card");
+		button_ScanCard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String selectedBarcodeString = (String) comboBox_MemberCard_Barcodes.getSelectedItem();
+				Barcode selectedMemberBarcode = null;
+				System.out.println("(EnterMemberNumber GUI) selected barcode " + selectedBarcodeString);
+				//Find this barcode in the list given in Simulation Class (as static list)
+				for (Barcode memberBarcode : MembershipDatabase.MEMBER_BARCODES.keySet())
+				{
+					if(memberBarcode.toString().equals(selectedBarcodeString))
+					{
+						selectedMemberBarcode = memberBarcode;
+						break;
+					}
+				}
+				//Scan the selected member barcode with the scanner of the diy system
+				if (station.mainScanner.scan(new BarcodedItem(selectedMemberBarcode, 0.1))) {
+					//scan successfully
+					System.out.println("(EnterMemberNumberGUI) Scan successfully");
+					} else {
+					//scan Failed
+					System.out.println("(EnterMemberNumberGUI) Scan fail");
+				}
+			}});
+		button_ScanCard.setFont(new Font("Lucida Grande", Font.BOLD, 19));
+		//Scan Card Button (ends)
+
+		//Swipe Card Button (starts)
+		/*
+		 * Swipe Card button, once pressed, gets the value from the combo box field, gets the cards from the customer wallet from Simulation class, swipes that card into card reader and display the card-holder name onto the scan GUI.
+		 *
+		 * We are getting the name of customer from the database after swiping the card at the card-reader
+		 * Card reader swipes the card, if the kind of card is "Membership" then we get the data without putting any hold
+		 * in CardReaderListener,
+		 * Swipe method in Card Reader returns the card data, and we get the card-holder name from it
+		 */
+		JButton button_SwipeCard = new JButton("Swipe Card");
+		button_SwipeCard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Make customer select the card
+				String selectedCard = (String) comboBox_MemberCardsInWallet.getSelectedItem();
+				System.out.println("(EnterMemberNumberGUI) Card selected " + selectedCard);
+				//John Member-Card , 99999999
+				//Split this to get card number
+				String[] selectedCardSplit = selectedCard.split(" , ");
+				String selectedCardNumber  = selectedCardSplit[1];
+				//System.out.println("Card selected Numberrr" + selectedCardNumber);
+				Card card = new Card("Membership", selectedCardNumber, "Membership Holder", "000", "000", true, true);
+				try {
+					station.cardReader.swipe(card);
+					System.out.println("Swipe successful");
+				} catch (IOException e1) {
+					e1.printStackTrace();
+					System.out.println("Swipe failed");
+				}
+			}
+		});
+		button_SwipeCard.setFont(new Font("Lucida Grande", Font.BOLD, 19));
+				
+		membership.add(label_MemberCards_Text);
+		membership.add(label_separator1);
+		membership.add(comboBox_MemberCardsInWallet);
+		membership.add(button_SwipeCard);
+		membership.add(label_separator2);
+		membership.add(label_Barcode_Text);
+		membership.add(label_separator3);
+		membership.add(comboBox_MemberCard_Barcodes);
+		membership.add(button_ScanCard);
+		
+		
 		container.add(cart);
 		container.add(placeList);
 		container.add(scales);
 		container.add(cash);
 		container.add(wallet);
+		container.add(membership);
 		container.setBorder(BorderFactory.createEmptyBorder(16, 16, 16, 16));
 		
 		customerSim.getContentPane().add(container);
